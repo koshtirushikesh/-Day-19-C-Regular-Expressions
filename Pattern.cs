997e315627c5cad2412e18c93543a86e39f84882
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2023-01-02 16:05:58 +0530

    validate first name

---
+++ Pattern.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexDemo
+{
+    internal class Pattern
+    {
+        
+        public static string User_Name = "^[A-Z]{1}[a-z]{2,9}$";
+
+        public bool validateUserName(string UserName)
+        {
+            return Regex.IsMatch(UserName, User_Name);
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace RegexDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern pattern = new Pattern();
+
+            Console.WriteLine("Enter a First Name:");
+            string UserFirstName = Console.ReadLine();
+
+            if (pattern.validateUserName(UserFirstName))
+            {
+                Console.WriteLine(" valid First Name ");
+            }
+            else
+            {
+                Console.WriteLine(" not a valid First Name ");
+            }
+        }
+    }
+}
